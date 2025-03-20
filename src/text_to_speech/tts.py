f7a99843c7a014140e6c7f9f9d422d2bdee4ce1d
b3W <basile.fouquet@epitech.eu>
2025-03-20 15:26:31 +0100

    fix(torch.load): override temorary torch.load to force param weight_only=False to allow loading of unsafe model, because torch 2.6 disable this.
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

--- src/text_to_speech/tts.py
+++ src/text_to_speech/tts.py
@@ -5,10 +5,24 @@ from pydub.playback import play
 
 class TextToSpeech:
     def __init__(self, model_name: str) -> None:
+
+        import torch
+
+        original_torch_load = torch.load
+
+        def custom_torch_load(*args, **kwargs): # temportary fix because idk
+            kwargs["weights_only"] = False
+            return original_torch_load(*args, **kwargs)
+
+        torch.load = custom_torch_load
+
         self.tts = TTS(
             model_name=model_name.replace('--', '/')
         )
+
         print("audio device:", sd.default.device)
+
+        torch.load = original_torch_load
 
     def __play_sound(self, path: str) -> None:
         song = AudioSegment.from_wav(path)
