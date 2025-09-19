cd381f460d94d7627b5c7212b028602b7f951131
Popochounet <adrien.audiard@epitech.eu>
2025-09-19 10:57:28 +0200

    evol(EventRouter): logging implemented

--- src/module/event_router.py
+++ src/module/event_router.py
@@ -1,11 +1,15 @@
+from typing import Optional
+
 import zmq
+
+from src.tools.logger import logging, setup_logger
 
 XPUB_ENDPOINT = "tcp://localhost:5555"
 XSUB_ENDPOINT = "tcp://localhost:5556"
 
 
 class EventRouter:
-    def __init__(self):
+    def __init__(self, logger: Optional[logging.Logger] = setup_logger("EventRouter")):
 
         self.ctx = zmq.Context()
         self.xpub = self.ctx.socket(zmq.XPUB)
@@ -13,6 +17,12 @@ class EventRouter:
         self.xpub.bind(XPUB_ENDPOINT)
         self.xsub.bind(XSUB_ENDPOINT)
 
+        self.logger = logger
+
     def start(self):
-        print("EventRouter started")
-        zmq.proxy(self.xsub, self.xpub)
+        try:
+            zmq.proxy(self.xsub, self.xpub)
+        except KeyboardInterrupt:
+            self.logger.info("Ctrl+C pressed, exiting cleanly")
+        except Exception as e:
+            self.logger.error(e)
