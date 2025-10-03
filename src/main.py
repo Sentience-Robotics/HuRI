d0fbae4983f2bb8d3483175de8e953dc81c20d99
Popochounet <adrien.audiard@epitech.eu>
2025-10-03 09:35:15 +0200

    evol(main): update main to demo new modules

--- src/main.py
+++ src/main.py
@@ -1,48 +1,44 @@
 import time
-from multiprocessing.synchronize import Event
 from typing import Dict
 
 from src.module.module import Module
 from src.module.shell import ModuleManager, RobotShell
+from src.module.speech_to_text.record_speech import RecordSpeech
+from src.module.speech_to_text.speech_to_text import SpeechToText
 from src.tools.logger import logging
 
 
 class TTSModule(Module):
-    def __init__(self, name="TTS", logger=None):
-        super().__init__(name, logger)
+    def __init__(self):
+        super().__init__()
+
+    def set_subscriptions(self) -> None:
         self.subscribe("llm.response", self.on_llm_response)
 
-    def on_llm_response(self, msg):
-        self.logger.debug(f"[{self.name}] parle -> {msg}")
+    def on_llm_response(self, msg: str):
+        self.logger.debug(f"parle -> {msg}")
 
 
 class LLMModule(Module):
-    def __init__(self, name="LLM", logger=None):
-        super().__init__(name, logger)
-        self.subscribe("speech.in", self.on_speech)
+    def __init__(self):
+        super().__init__()
 
-    def on_speech(self, msg):
+    def set_subscriptions(self) -> None:
+        self.subscribe("text.in", self.on_speech)
+
+    def on_speech(self, msg: str):
         reponse = f"Réponse à '{msg}'"
-        self.logger.debug(f"[{self.name}] -> {reponse}")
+        self.logger.debug(f"{reponse}")
         self.publish("llm.response", reponse)
 
 
-class STTModule(Module):
-    def __init__(self, name="STT", logger=None):
-        super().__init__(name, logger)
-
-    def loop(self, stop_event: Event = None):
-        i = 0
-        while stop_event is None or not stop_event.is_set():
-            phrase = f"Phrase numéro {i}"
-            self.logger.debug(f"[{self.name}] -> {phrase}")
-            self.publish("speech.in", phrase)
-            i += 1
-            time.sleep(2)
-
-
 if __name__ == "__main__":
-    modules: Dict[str, Module] = {"STT": STTModule, "TTS": TTSModule, "LLM": LLMModule}
+    modules: Dict[str, Module] = {
+        "REC": RecordSpeech(),
+        "STT": SpeechToText(),
+        "LLM": LLMModule(),
+        "TTS": TTSModule(),
+    }
     manager = ModuleManager(modules)
     manager.start()
     time.sleep(0.1)
