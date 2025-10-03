d64f038ccc3b2ee340900efad1a63620d23e7fdb
Popochounet <adrien.audiard@epitech.eu>
2025-10-03 09:31:10 +0200

    evol(Module): added other event struct type (json & bytes)

--- src/module/module.py
+++ src/module/module.py
@@ -1,86 +1,116 @@
-import multiprocessing as mp
+import json
 import threading
-import time
+from abc import ABC, abstractmethod
 from multiprocessing.synchronize import Event
-from typing import Callable, Dict, Optional
+from typing import Callable, Dict, final
 
 import zmq
 
-from src.tools.logger import logging, setup_logger
+from src.tools.logger import logging
 
 from .event_router import XPUB_ENDPOINT, XSUB_ENDPOINT
 
 
-class Module:
-    def __init__(self, name: str, logger: Optional[logging.Logger] = None) -> None:
-        self.name = name
+class Module(ABC):
+    def __init__(self):
+        """Child Modules must call super.__init__() in their __init__() function."""
+        self.ctx = None
+        self.pub_socket = None
+        self.subs: Dict[str, zmq.Socket[bytes]] = {}
+        self.callbacks = {}
+        self._poller_running = False
+        self.poller = None
+        self.logger = logging.getLogger(__name__)
+
+    @final
+    def _initialize(self) -> None:
+        """
+        Called inside start_module() or manually before usage.
+        This function exist because ctx cannot be set in __init__, because of multi-processing.
+        """
         self.ctx = zmq.Context()
         self.pub_socket = self.ctx.socket(zmq.PUB)
         self.pub_socket.connect(XSUB_ENDPOINT)
-
-        self.subs: Dict[str, zmq.SyncSocket] = {}
-        self.callbacks: Dict[str, Callable] = {}
-        self._running = False
         self.poller = threading.Thread(target=self._poll_loop, daemon=True)
+        self.set_subscriptions()
 
-        self.logger = logger or logging.getLogger(self.name)
+    @abstractmethod
+    def set_subscriptions(self) -> None:
+        """Child module must define this funcction with subscriptions"""
+        ...
 
+    @final
     def subscribe(self, topic: str, callback: Callable) -> None:
         sub_socket = self.ctx.socket(zmq.SUB)
         sub_socket.connect(XPUB_ENDPOINT)
         sub_socket.setsockopt_string(zmq.SUBSCRIBE, topic)
         self.subs[topic] = sub_socket
         self.callbacks[topic] = callback
+        self.logger.info(f"Subscribe: {topic}")
 
-    def publish(self, topic: str, msg: str) -> None:
-        self.pub_socket.send_multipart([topic.encode(), msg.encode()])
-        self.logger.info(f"Publish: {topic} {msg}")
+    @final
+    def publish(
+        self, topic: str, msg: object, content_type: str = "str"
+    ) -> None:  # TODO content type enum
+        if content_type == "json":
+            payload = json.dumps(msg).encode()
+        elif content_type == "bytes":
+            payload = msg
+        elif content_type == "str":
+            payload = msg.encode()
+        else:
+            raise ValueError(f"Unsupported content_type: {content_type}")
 
-    def start_polling(self) -> None:
-        self._running = True
+        self.pub_socket.send_multipart([topic.encode(), content_type.encode(), payload])
+        self.logger.info(f"Publish: {topic} {content_type}")
+
+    @final
+    def _start_polling(self) -> None:
+        self._poller_running = True
         self.poller.start()
 
+    @final
     def _poll_loop(self) -> None:
         poller = zmq.Poller()
         for sub in self.subs.values():
             poller.register(sub, zmq.POLLIN)
 
-        while self._running:
+        while self._poller_running:
             events = dict(poller.poll(100))
             for _, sub in self.subs.items():
                 if sub in events:
-                    topic, msg = sub.recv_multipart()
+                    topic, content_type, payload = sub.recv_multipart()
                     topic_str = topic.decode()
-                    msg_str = msg.decode()
-                    self.logger.info(f"Receive: {topic_str} {msg_str}")
-                    self.callbacks[topic_str](msg_str)
+                    content_type_str = content_type.decode()
+                    if content_type_str == "json":
+                        data = json.loads(payload.decode())
+                    elif content_type_str == "bytes":
+                        data = payload
+                    elif content_type_str == "str":
+                        data = payload.decode()
+                    self.logger.info(f"Receive: {topic_str} {content_type_str}")
+                    self.callbacks[topic_str](data)
 
-    def run(self, stop_event: Event = None) -> None:
-        """
-        Default run: waits for events.
-        Child classes can override `loop()` for active behavior.
-        """
+    @final
+    def start_module(self, stop_event: Event = None) -> None:
+        self._initialize()
         if self.subs != {}:
-            self.start_polling()
+            self._start_polling()
         try:
-            self.loop(stop_event)
+            self.run_module(stop_event)
         except KeyboardInterrupt:
             self.logger.info("Ctrl+C pressed, exiting cleanly")
         except Exception as e:
             self.logger.error(e)
         finally:
-            self.stop()
+            self.stop_module()
 
-    def loop(self, stop_event: Event = None) -> None:
-        """Child modules override this instead of run(). Default: idle wait."""
-        while stop_event is None or not stop_event.is_set():
-            time.sleep(0.1)
-
-    def stop(self) -> None:
+    @final
+    def stop_module(self) -> None:
         """Stop the module gracefully."""
 
-        if self._running:
-            self._running = False
+        if self._poller_running:
+            self._poller_running = False
             self.poller.join()
 
         for topic, sub in self.subs.items():
@@ -103,3 +133,13 @@ class Module:
             self.logger.error(f"Error terminating ZMQ context: {e}")
 
         self.logger.info(f"Module stopped gracefully.")
+
+    def run_module(self, stop_event: Event = None) -> None:
+        """Child modules override this instead of run(). Default: idle wait."""
+        if stop_event:
+            stop_event.wait()
+
+    @final
+    def set_custom_logger(self, logger) -> None:
+        """The default logger in set in __init__."""
+        self.logger = logger
