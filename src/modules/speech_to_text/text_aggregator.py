63d5910cda50091b73122515440ccd00f4b88956
Popochounet <adrien.audiard@epitech.eu>
2026-05-03 18:14:41 +0200

    fix(TAG): some match would destroy sentence

--- src/modules/speech_to_text/text_aggregator.py
+++ src/modules/speech_to_text/text_aggregator.py
@@ -30,18 +30,23 @@ class TAG(Module):
         super().__init__()
 
         self.sentence: str = ""
+        self.prev_index: int = 0
 
     def _merge(self, current: str, new: str) -> str:
         matcher = SequenceMatcher(None, current, new)
         match = matcher.find_longest_match(0, len(current), 0, len(new))
+        if match.a >= self.prev_index:
+            self.prev_index = match.a
+            return current[: match.a] + new[match.b :]
 
-        return current[: match.a] + new[match.b :]
+        self.prev_index = len(current)
+        return current + new
 
     async def process(self, transcript: Transcript) -> Optional[Sentence]:
         text = transcript.text
 
         if text != "":
-            if not self.sentence:
+            if self.sentence == "":
                 self.sentence = text
             else:
                 self.sentence = self._merge(self.sentence, text)
@@ -49,6 +54,7 @@ class TAG(Module):
         if transcript.end and self.sentence != "":
             sentence = Sentence(self.sentence)
             self.sentence = ""
+            self.prev_index = 0
             return sentence
         else:
             return None
