2026836812523a20b53d412ff6a898b69616cad8
Popochounet <adrien.audiard@epitech.eu>
2026-05-03 12:47:15 +0200

    feat(TAG): aggregate all transcription into one question

---
+++ src/modules/speech_to_text/text_aggregator.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from typing import Optional
+from .speech_to_text import Transcript
+
+from src.core.module import Module
+
+from difflib import SequenceMatcher
+
+
+@dataclass
+class Sentence:
+    text: str
+
+
+class TAG(Module):
+    """TAG Module
+
+    Aggregate all transcriptions and send when transcript end.
+
+    input: transcript,
+    output: question
+    """
+
+    input_type = "transcript"
+    output_type = "question"
+
+    def __init__(
+        self,
+    ):
+        super().__init__()
+
+        self.sentence: str = ""
+
+    def _merge(self, current: str, new: str) -> str:
+        matcher = SequenceMatcher(None, current, new)
+        match = matcher.find_longest_match(0, len(current), 0, len(new))
+
+        return current[: match.a] + new[match.b :]
+
+    async def process(self, transcript: Transcript) -> Optional[Transcript]:
+        text = transcript.text
+
+        if text != "":
+            if not self.sentence:
+                self.sentence = text
+            else:
+                self.sentence = self._merge(self.sentence, text)
+
+        if transcript.end and self.sentence != "":
+            sentence = Sentence(self.sentence)
+            self.sentence = ""
+            return sentence
+        else:
+            return None
