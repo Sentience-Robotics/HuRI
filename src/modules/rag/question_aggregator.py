0d9ab915a00e0afc87c46b5213c2a8dcf51244fc
Popochounet <adrien.audiard@epitech.eu>
2026-06-14 11:17:28 +0200

    feat(rag): question aggregator module (QAG); aggregate emotion and transcript into RAGQuestion

---
+++ src/modules/rag/question_aggregator.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+from src.core.module import Module
+from src.modules.emotion.events import Emotion
+from src.modules.speech_to_text.events import Transcript
+
+from .events import PartialQuestion, RAGQuestion
+
+
+class QAG(Module):
+    """QAG Module
+
+    Aggregate sentence and emotion into a RAGQuestion.
+
+    input: partial_question,
+    output: question
+    """
+
+    input_type = "partial_question"
+    output_type = "question"
+
+    def __init__(self, use_emotion: bool = True):
+        super().__init__()
+
+        self.current_transcript: Optional[Transcript] = None
+        self.current_emotion: Optional[Emotion] = None
+
+        self.use_emotion = use_emotion
+
+    async def process(self, partial_question: PartialQuestion) -> Optional[RAGQuestion]:
+        if partial_question.emotion is not None:
+            self.current_emotion = partial_question.emotion
+
+        if partial_question.transcript is not None:
+            self.current_transcript = partial_question.transcript
+
+        if self.current_transcript is not None:
+            if self.use_emotion:
+                if self.current_emotion is not None:
+                    return RAGQuestion(self.current_transcript, self.current_emotion)
+            else:
+                return RAGQuestion(self.current_transcript, None)
+
+        return None
