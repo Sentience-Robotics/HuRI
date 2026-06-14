b10495ada06c71a6faaad136bff07f963d245138
Popochounet <adrien.audiard@epitech.eu>
2026-06-14 11:16:44 +0200

    feat(emotion): emotion aggregator module (EAG)

---
+++ src/modules/emotion/emotion_aggregator.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+from typing import Optional
+
+from src.core.module import Module
+from src.modules.rag.events import PartialQuestion
+
+from .events import Emotion
+
+
+class EAG(Module):
+    """EAG Module
+
+    Aggregate all emotions and send when voice end.
+
+    input: emotion,
+    output: partial_question
+    """
+
+    input_type = "emotion"
+    output_type = "partial_question"
+
+    def __init__(self, ema_alpha: Optional[float] = None):
+        super().__init__()
+
+        self.scores = defaultdict(float)
+        self.count = 0
+
+        self.ema_alpha = ema_alpha
+
+    def _finalize(self) -> Emotion:
+        avg_scores = (
+            {label: score / self.count for label, score in self.scores.items()}
+            if self.ema_alpha is None
+            else self.scores
+        )
+
+        best_label = max(avg_scores, key=avg_scores.get)
+
+        result = Emotion(
+            label=best_label,
+            confidence=avg_scores[best_label],
+            scores=avg_scores,
+            end=True,
+        )
+
+        self.scores.clear()
+        self.count = 0
+
+        return result
+
+    async def process(self, emotion: Emotion) -> Optional[PartialQuestion]:
+        if self.ema_alpha is not None:
+            for label, score in emotion.scores.items():
+                self.scores[label] = (
+                    self.ema_alpha * score + (1 - self.ema_alpha) * self.scores[label]
+                )
+        else:
+            for label, score in emotion.scores.items():
+                self.scores[label] += score
+
+        self.count += 1
+
+        if emotion.end:
+            emotion = self._finalize()
+
+            return PartialQuestion(transcript=None, emotion=emotion)
+
+        return None
--- src/modules/modules.py
+++ src/modules/modules.py
@@ -1,6 +1,8 @@
 from typing import Dict, Type
 
+from src.modules.emotion.emotion_aggregator import EAG
 from src.modules.emotion.prosody_analysis import EMO
+from src.modules.rag.question_aggregator import QAG
 from src.modules.rag.rag import RAG
 from src.modules.speech_to_text.microphone_vad import MIC
 from src.modules.speech_to_text.speech_to_text import STT
@@ -17,6 +19,9 @@ def get_modules() -> Dict[str, Type[Module]]:
         "stt": STT,
         "tag": TAG,
         "emo": EMO,
+        "rag": RAG,
+        "eag": EAG,
+        "qag": QAG,
         "rag": RAG,
         "tts": TTS,
         "gesture": Gesture,
